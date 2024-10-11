70731d60fae2669cb00cfe0cc18db2bb96efeec1
devanidarshan <devanidarshan333@gmail.com>
2024-10-11 12:21:04 -0700

    Crud-Mysql-Postman

--- src/controller/cart.controller.js
+++ src/controller/cart.controller.js
@@ -1,3 +1,4 @@
+const common = require('../../common');
 const mysqlConnection = require('../../connection');
 
 // ADD TO CART
@@ -5,10 +6,11 @@ exports.addToCart = (req, res) => {
     const { productId, quantity } = req.body;
     const buyerId = req.query.id;
     // console.log(buyerId);
-    
+
     if (!productId || !quantity) {
         return res.status(400).json({ message: 'Product ID and quantity are required.' });
     }
+
 
     const insertQuery = 'INSERT INTO cart (productId, quantity, buyerId) VALUES (?, ?, ?)';
     mysqlConnection.query(insertQuery, [productId, quantity, buyerId], (err, result) => {
@@ -30,17 +32,27 @@ exports.addToCart = (req, res) => {
 };
 
 // GET ALL CART
-exports.getAllCart= (req, res) => {
-    const query = 'SELECT * FROM cart';
-    mysqlConnection.query(query, (err, results) => {
-        if (err) {
-            console.error(err);
-            return res.status(500).json({ message: 'Error fetching cart items.' });
+exports.getAllCart = (req, res) => {
+    const sql = 'SELECT * FROM product';
+    mysqlConnection.query(sql, function (error, products) {
+        if (error) {
+            console.log(error);
+            return common.reply(res, 500, true, "Database Error...");
         }
-        res.render('cart', { cartItems: results });
-        console.log(results);
+
+        const query = 'SELECT * FROM cart';
+        mysqlConnection.query(query, (err, cartItems) => {
+            if (err) {
+                console.error(err);
+                return res.status(500).json({ message: 'Error fetching cart items.' });
+            }
+            
+            // Pass cartItems and products to the view
+            res.render('cart', { cartItems, products });
+        });
     });
 };
+
 
 // REMOVE CART
 exports.removeCart = (req , res) => {
